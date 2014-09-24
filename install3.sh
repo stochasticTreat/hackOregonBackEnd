b1a947949cd19eddcc3c457eff9da4160a0290ec
samhiggins2001 <higgins2001@hotmail.com>
2014-09-23 23:40:31 -0700

    fixed mispelling in call to build out from git repo

--- install3.sh
+++ install3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "running install3.sh"
+echo "current working directory:"
+pwd
 echo "deb http://http://ftp.osuosl.org/pub/cran/bin/linux/ubuntu trusty/" >> ./sources.list.appendme
 sudo cat /etc/apt/sources.list ./sources.list.appendme  > ./sources.list.tmp
 sudo mv ./sources.list.tmp /etc/apt/sources.list
@@ -28,11 +31,14 @@ else
 	wget  http://s3-us-west-2.amazonaws.com/mp-orestar-dump/hackoregon.sql.bz2
 fi
 
+echo "Current working directory:"
+pwd
+
 sudo mkdir ~/data_infrastructure
 
 sudo cp ./hackoregon.sql.bz2 ~/data_infrastructure/hackoregon.sql.bz2
-
-sudo ./buildoutFromGitRepo.sh
+sudo chmod 777 ./buildoutFromGitRepo.sh
+sudo ./buildOutFromGitRepo.sh
 
 cd ~
 cwd=$(pwd)
@@ -55,7 +61,8 @@ sudo -u postgres createlang plpgsql
 
 sudo -u postgres psql -c "alter user postgres password 'points';"
 
+sudo chmod 777 ./orestar_scrape/bulkAddTransactions.R
 sudo ./orestar_scrape/bulkAddTransactions.R ~/data_infrastructure/successfullyMerged/joinedTables.tsv skipRebuild
-
+sudo chmod 777 ./buildOutDBFromRawTables.sh
 sudo ./buildOutDBFromRawTables.sh
 
