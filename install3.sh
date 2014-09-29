554382d3a56b5f00e47be202406a63b3caa782f3
samhiggins2001 <higgins2001@hotmail.com>
2014-09-29 14:48:48 -0700

    scraper, endpoints, chmods, installation

--- install3.sh
+++ install3.sh
@@ -4,7 +4,7 @@ echo "current working directory:"
 pwd
 echo "deb http://http://ftp.osuosl.org/pub/cran/bin/linux/ubuntu trusty/" >> ./sources.list.appendme
 sudo cat /etc/apt/sources.list ./sources.list.appendme  > ./sources.list.tmp
-sudo mv ./sources.list.tmp /etc/apt/sources.list
+sudo cp ./sources.list.tmp /etc/apt/sources.list
 rm ./sources.list.appendme
 
 sudo apt-get update
@@ -13,8 +13,8 @@ sudo apt-get -y install r-base-dev
 
 #create a R library for the user:
 echo R_LIBS_USER=\"~/lib/R/library\" > ~/.Renviron
-mkdir ~/lib/R/library
-sudo cp ./Rprofile .Rprofile
+sudo mkdir ~/lib/R/library
+sudo cp ./Rprofile ~/.Rprofile
 
 #install java so that some r packages will work.
 #https://www.digitalocean.com/community/tutorials/how-to-install-java-on-ubuntu-with-apt-get
@@ -37,13 +37,14 @@ pwd
 sudo mkdir ~/data_infrastructure
 
 sudo cp ./hackoregon.sql.bz2 ~/data_infrastructure/hackoregon.sql.bz2
-sudo chmod 777 ./buildoutFromGitRepo.sh
+sudo chmod 755 ./buildoutFromGitRepo.sh
 sudo ./buildOutFromGitRepo.sh
 
-cd ~
-cwd=$(pwd)
-datadir="${cwd}/data_infrastructure"
-cd $datadir
+# cd ~
+# cwd=$(pwd)
+# datadir="${cwd}/data_infrastructure"
+# cd $datadir
+cd ~/data_infrastructure
 
 echo "install3.sh changed the working directory to:"
 pwd
@@ -61,8 +62,8 @@ sudo -u postgres createlang plpgsql
 
 sudo -u postgres psql -c "alter user postgres password 'points';"
 
-sudo chmod 777 ./orestar_scrape/bulkAddTransactions.R
+sudo chmod 755 ./orestar_scrape/bulkAddTransactions.R
 sudo ./orestar_scrape/bulkAddTransactions.R ~/data_infrastructure/successfullyMerged/joinedTables.tsv skipRebuild
-sudo chmod 777 ./buildOutDBFromRawTables.sh
+sudo chmod 755 ./buildOutDBFromRawTables.sh
 sudo ./buildOutDBFromRawTables.sh
 
